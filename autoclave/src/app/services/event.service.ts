c3524514ab72085d6d6cf1f3639df1efad8a19af
Brayden Winterton <bwinterton@gmail.com>
2019-08-05 10:57:16 -0600

    Add WS reconnect logic to Autoclave

--- autoclave/src/app/services/event.service.ts
+++ autoclave/src/app/services/event.service.ts
@@ -5,15 +5,34 @@ import { Injectable, EventEmitter } from "@angular/core";
 })
 export class EventService {
   private listener: EventEmitter<any>;
+  private retryTimer: number;
 
   constructor() {
-    const endpoint = "ws://" + window.location.host + "/websocket";
-    const ws = new WebSocket(endpoint);
     this.listener = new EventEmitter();
+    this.openWebsocket();
+  }
+
+  openWebsocket() {
+    const endpoint = "ws://" + window.location.host + "/websocket";
+    let ws = new WebSocket(endpoint);
+
+    ws.onopen = () => {
+      clearInterval(this.retryTimer);
+      this.retryTimer = 0;
+    };
 
     ws.onmessage = event => {
       console.log("Emmitting event: " + event);
       this.listener.emit(event);
+    };
+
+    ws.onclose = () => {
+      ws = null;
+      if (!this.retryTimer) {
+        this.retryTimer = window.setInterval(() => {
+          this.openWebsocket();
+        }, 1000);
+      }
     };
   }
 
